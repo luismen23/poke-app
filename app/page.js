26352d9ac9fc46b2950a0b6a311ad7d7f25d74ff
Luis Mendoza <lmmg1701@gmail.com>
2024-03-07 21:10:03 -0400

    improving first charge

--- app/page.js
+++ app/page.js
@@ -1,42 +1,68 @@
+import SearchBar from '@/components/SearchBar';
+import Pagination from '@/components/Pagination';
+import PokemonTable from '@/components/PokemonTable';
+import { fetchPokemon, fetchUrl } from '@/scripts/data';
+import { Suspense } from 'react';
 
-import SearchBar from "@/components/SearchBar";
-import Pagination from "@/components/Pagination";
-import PokemonTable from "@/components/PokemonTable";
-import { fetchPokemon, fetchUrl } from "@/scripts/data";
-import { Suspense } from "react";
+const mapPageIndexToPokemonRange = {
+  1: {
+    startIndex: 0,
+    endIndex: 40,
+  },
+  2: {
+    startIndex: 40,
+    endIndex: 85,
+  },
+  3: {
+    startIndex: 80,
+    endIndex: 120,
+  },
+};
 
+export default async function Home({ searchParams }) {
+  const currentPage = Number(searchParams?.page) || 1;
+  const search = (await searchParams?.search) || '';
 
-export default async function Home({searchParams}) {
+  const pokemons = await fetchPokemon(151, 0);
 
-  const currentPage = Number(searchParams?.page) || 1
-  const search = await searchParams?.search || ''
+  const startIndex = mapPageIndexToPokemonRange[currentPage].startIndex;
+  const endIndex = mapPageIndexToPokemonRange[currentPage].endIndex;
+  const pokemonImg = pokemons
+    .slice(startIndex, endIndex)
+    .map(async (pokemon, index) => {
+      const pokeData = await fetchUrl(pokemon.url);
+      console.log('Me estoy ejecutando!!', index + startIndex);
+      const image = pokeData.sprites?.front_default;
+      const { name, id, types, stats } = pokeData;
+      return { name, id, types, stats, image };
+    });
 
-  const pokemons = await fetchPokemon(151, 0)
-  const pokemonImg = pokemons.map(async (pokemon) => {
-    const pokeData = await fetchUrl(pokemon.url)
-    const image = pokeData.sprites?.other['official-artwork']?.front_default
-    const {name, id, types, stats} = pokeData
-      return {name, id, types, stats, image}
-    })
-  const pokemonData = await Promise.all(pokemonImg)
-  
-  const dataLength = pokemonData.length
-  const per_page = 40
-  const totalPages = Math.ceil(dataLength / per_page)
+  const pokemonData = await Promise.all(pokemonImg);
 
-  const start = (Number(currentPage) - 1) * per_page
-  const end = start + per_page
-  const entries = pokemonData.slice(start, end)
-  
+  const dataLength = 151;
+  const per_page = 40;
+  const totalPages = Math.ceil(dataLength / per_page);
+
+  const start = (Number(currentPage) - 1) * per_page;
+  const end = start + per_page;
+
   return (
-    <main className="flex flex-col items-center w-screen h-screen pt-32 gap-5"> 
-      <SearchBar response={pokemonData}/>
-      
+    <main className='flex flex-col items-center w-screen h-screen pt-32 gap-5'>
+      <SearchBar response={pokemonData} />
+
       <Suspense key={search + currentPage} fallback='Loading Pokemon'>
-        <PokemonTable search={search} pokemonData={entries} currentPage={currentPage}/>
+        <PokemonTable
+          search={search}
+          pokemonData={pokemonData}
+          currentPage={currentPage}
+        />
       </Suspense>
       <Suspense fallback='pagination'>
-        <Pagination dataLength={dataLength} totalPages={totalPages} per_page={per_page}/>
+        <Pagination
+          dataLength={dataLength}
+          totalPages={totalPages}
+          per_page={per_page}
+        />
       </Suspense>
     </main>
   );
--- components/PokemonTable.jsx
+++ components/PokemonTable.jsx
@@ -1,88 +1,100 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Suspense } from "react"
+import Image from 'next/image';
+import Link from 'next/link';
+import { Suspense } from 'react';
 
 export const changeType = (type) => {
-    return type === 'grass' && '/img/grass.webp' ||
-    type === 'poison' && '/img/poison.webp' ||
-    type === 'fire' && '/img/fire.webp' ||
-    type === 'water' && '/img/water.webp' || 
-    type === 'bug' && '/img/bug.webp' ||
-    type === 'flying' && '/img/flying.webp' ||
-    type === 'normal' && '/img/normal.webp' ||
-    type === 'electric' && '/img/electric.webp' || 
-    type === 'ground' && '/img/ground.webp' || 
-    type === 'fairy' && '/img/fairy.webp' || 
-    type === 'fighting' && '/img/fighting.webp' || 
-    type === 'psychic' && '/img/psychic.webp' ||
-    type === 'steel' && '/img/steel.webp' || 
-    type === 'ice' && '/img/ice.webp' || 
-    type === 'rock' && '/img/rock.webp' || 
-    type === 'dragon' && '/img/dragon.webp' || 
-    type === 'dark' && '/img/dark.webp' || 
-    type === 'ghost' && '/img/ghost.webp' 
-}
+  return (
+    (type === 'grass' && '/img/grass.webp') ||
+    (type === 'poison' && '/img/poison.webp') ||
+    (type === 'fire' && '/img/fire.webp') ||
+    (type === 'water' && '/img/water.webp') ||
+    (type === 'bug' && '/img/bug.webp') ||
+    (type === 'flying' && '/img/flying.webp') ||
+    (type === 'normal' && '/img/normal.webp') ||
+    (type === 'electric' && '/img/electric.webp') ||
+    (type === 'ground' && '/img/ground.webp') ||
+    (type === 'fairy' && '/img/fairy.webp') ||
+    (type === 'fighting' && '/img/fighting.webp') ||
+    (type === 'psychic' && '/img/psychic.webp') ||
+    (type === 'steel' && '/img/steel.webp') ||
+    (type === 'ice' && '/img/ice.webp') ||
+    (type === 'rock' && '/img/rock.webp') ||
+    (type === 'dragon' && '/img/dragon.webp') ||
+    (type === 'dark' && '/img/dark.webp') ||
+    (type === 'ghost' && '/img/ghost.webp')
+  );
+};
 
-export default async function PokemonTable({pokemonData}) {
+export default async function PokemonTable({ pokemonData }) {
+  return (
+    <table className='w-[20rem] md:w-[40rem] table relative'>
+      <thead className='table-header-group text-left '>
+        <tr className='bg-slate-900 '>
+          <th className=' p-2'>#</th>
+          <th className=' p-2'>Name</th>
+          <th className=' hidden md:table-cell p-2'>Attack</th>
+          <th className=' hidden md:table-cell p-2'>Defense</th>
+          <th className=' hidden md:table-cell p-2'>Stamina</th>
+        </tr>
+      </thead>
 
-    return (
-        <table className="w-[20rem] md:w-[40rem] table relative">
-                <thead className="table-header-group text-left ">
-                    <tr className="bg-slate-900 ">
-                        <th className=" p-2">#</th>
-                        <th className=" p-2">Name</th>
-                        <th className=" hidden md:table-cell p-2">Attack</th>
-                        <th className=" hidden md:table-cell p-2">Defense</th>
-                        <th className=" hidden md:table-cell p-2">Stamina</th>
-                    </tr>
-                </thead>
-                
-                <tbody className="table-row-group">                
-                    {
-                        pokemonData.map((pokemon, pokemonId) => {
-                            const types = pokemon?.types.map((type)=>{
-                                return type.type.name
-                            })
-                            
-                            const stats = pokemon?.stats.map(({base_stat})=>{
-                                return base_stat
-                            })
-                            return (
-                                <Suspense key={pokemonId} fallback='loading pokemon' >
-                                    <tr className="bg-slate-700 even:bg-slate-800 ">
-                                        <td className=" table-cell p-2">{pokemon?.id}</td>
-                                        <td className=" table-cell p-2">
-                                            <Link href={`${pokemon?.name}`} className="flex items-center justify-between">
-                                                <span className="flex items-center gap-1">
-                                                    <span className="w-7 h-7 relative">
-                                                        <Image src={pokemon?.image} alt='img' fill sizes="auto" className="object-contain"/>
-                                                    </span>
-                                                    {pokemon?.name.charAt(0).toUpperCase() + pokemon?.name.slice(1)}
-                                                </span>
-                                                <span className="flex gap-1">
-                                                    {
-                                                        types.map((type) => {
-                                                            return (
-                                                                <span key={type} className=" w-6 h-6 relative">
-                                                                    <Image src={changeType(type)} fill sizes='auto' alt={type} className=""/>
-                                                                </span>
-                                                            )
-                                                        })
-                                                    }
-                                                </span>
-                                            </Link>
-                                        </td>
-                                        <td className=" hidden md:table-cell p-2">{stats?.[1]} ATK</td>
-                                        <td className=" hidden md:table-cell p-2">{stats?.[2]} DEF</td>
-                                        <td className=" hidden md:table-cell p-2">{stats?.[0]} HP</td>
-                                    </tr>
-                                </Suspense>
-                            )
-                        })
-                    }
-                </tbody>            
-                    
-            </table>
-    )
-    
+      <tbody className='table-row-group'>
+        {pokemonData.map((pokemon, pokemonId) => {
+          const types = pokemon?.types.map((type) => {
+            return type.type.name;
+          });
+
+          const stats = pokemon?.stats.map(({ base_stat }) => {
+            return base_stat;
+          });
+          return (
+            <Suspense key={pokemonId} fallback='loading pokemon'>
+              <tr className='bg-slate-700 even:bg-slate-800 '>
+                <td className=' table-cell p-2'>{pokemon?.id}</td>
+                <td className=' table-cell p-2'>
+                  <Link
+                    href={`${pokemon?.name}`}
+                    className='flex items-center justify-between'
+                  >
+                    <span className='flex items-center gap-1'>
+                      <span className='w-7 h-7 relative'>
+                        <Image
+                          src={pokemon?.image}
+                          alt='img'
+                          fill
+                          sizes='auto'
+                          className='object-contain'
+                          loading='lazy'
+                        />
+                      </span>
+                      {pokemon?.name.charAt(0).toUpperCase() +
+                        pokemon?.name.slice(1)}
+                    </span>
+                    <span className='flex gap-1'>
+                      {types.map((type) => {
+                        return (
+                          <span key={type} className=' w-6 h-6 relative'>
+                            <Image
+                              src={changeType(type)}
+                              fill
+                              sizes='auto'
+                              alt={type}
+                              className=''
+                            />
+                          </span>
+                        );
+                      })}
+                    </span>
+                  </Link>
+                </td>
+                <td className=' hidden md:table-cell p-2'>{stats?.[1]} ATK</td>
+                <td className=' hidden md:table-cell p-2'>{stats?.[2]} DEF</td>
+                <td className=' hidden md:table-cell p-2'>{stats?.[0]} HP</td>
+              </tr>
+            </Suspense>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
